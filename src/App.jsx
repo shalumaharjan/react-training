65f8c89fdaa61dc50dbae3fbb4ebfbde062bd89a
Shalu Maharjan <shalumaharjan15@gmail.com>
2025-12-26 13:00:43 +0545

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,39 @@ import "./App.css";
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [characterName, setCharacterName] = useState("");
-  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   // async: wait till another task not completed, fetching in bg
   const handleSubmit = async (event) => {
-    if (event.key === "Enter") {
-      try {
-        // template literals `: can use declared variable - making dynamic
-        // API call
-        const response = await fetch(
-          `https://yurippe.vercel.app/api/quotes?character=${characterName}&random=1`
-        );
-        const request = await response.json();
-        // console.log(response);
-        setQuotes(request);
-        console.log(request);
-      } catch (error) {
-        console.log(error);
+    event.preventDefault(); // form submitted, prevent reloading
+    console.log("Form Submitted");
+
+    // if (event.key === "Enter") {
+    try {
+      setLoading(true); //show loading while fetching
+      setError("");
+      setQuotes([]);
+
+      // template literals `: can use declared variable - making dynamic
+      // API call
+      const response = await fetch(
+        `https://yurippe.vercel.app/api/quotes?character=${characterName}&random=1`
+      );
+      const request = await response.json();
+      // console.log(response);
+
+      if (response.status === 404) {
+        throw new Error("Character not found");
       }
+      setQuotes(request);
+      // console.log(request);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
+    // }
   };
 
   // console.log(quotes);
@@ -30,22 +44,26 @@ function App() {
 
   return (
     <>
-      {/* <h1>Hi</h1> */}
-      <h2>Anime Quotes</h2>
-      <input
-        value={characterName}
-        placeholder="Anime"
-        onChange={(event) => setCharacterName(event.target.value)}
-        onKeyUp={handleSubmit}
-      />
+      <h1>Anime Quotes</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={characterName}
+          placeholder="Anime Character"
+          onChange={(event) => setCharacterName(event.target.value)}
+          // onKeyUp={handleSubmit}
+        />
+        <button type="Submit">Search</button>
+      </form>
+      {loading && <p>Loading.......</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       {/* <button onClick={useEffect}>Character</button> */}
       <ul>
         {quotes.map((item) => (
           <div key={item._id}>
-            <li>{item.quote}</li>
-            <li>{item.character}</li>
+            <p style={{ fontWeight: "bold" }}>{item.show}</p>
+            <p>{item.quote}</p>
+            <p style={{ fontWeight: "bold" }}>- {item.character}</p>
           </div>
-          // item.character
         ))}
       </ul>
     </>
